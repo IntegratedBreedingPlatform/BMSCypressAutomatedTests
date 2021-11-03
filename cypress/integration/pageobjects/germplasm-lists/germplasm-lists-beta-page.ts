373b45f9e2f11f3658cf1fe4d378a3e44bf25683
Nahuel Soldevilla <nahuel@leafnode.io>
2021-11-03 11:24:32 -0300

    IBP-5059: use Cypress.config('pageLoadTimeout') in first assertion

--- cypress/integration/pageobjects/germplasm-lists/germplasm-lists-beta-page.ts
+++ cypress/integration/pageobjects/germplasm-lists/germplasm-lists-beta-page.ts
@@ -4,9 +4,8 @@ export default class GermplasmListsBetaPage {
 
     openImportGermplasmListModal() {
         getIframeBody().then(($iframe) => {
-            cy.wrap($iframe).find('[data-test="actionMenu"]', { timeout: 15000 }).should('exist').click();
+            cy.wrap($iframe).find('[data-test="actionMenu"]', { timeout: Cypress.config('pageLoadTimeout') }).should('exist').click();
             cy.wrap($iframe).find('[data-test="importListButton"]').should('exist').click();
-
         })
     }
 
