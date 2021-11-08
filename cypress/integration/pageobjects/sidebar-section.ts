f6a1b5a382835da27b98e32c2ff0067420ee391e
Darla Grace Hermo <darla@leafnode.io>
2021-11-08 14:56:47 +0800

    Fix failing (when we passed environment variables by CLI) test on sidebar version checking
    IBP-5056

--- cypress/integration/pageobjects/sidebar-section.ts
+++ cypress/integration/pageobjects/sidebar-section.ts
@@ -40,7 +40,8 @@ export default class SidebarSection {
     }
 
     verifyBMSVersion() {
-        let bmsVersion = Cypress.env('bmsVersion').replace('.0','');
+        let bmsVersion = Cypress.env('bmsVersion');
+        bmsVersion.replace('.0','');
         cy.xpath(`//mat-sidenav/div[contains(text(), 'BMS ${bmsVersion}')]`).should('exist');
     }
 }
