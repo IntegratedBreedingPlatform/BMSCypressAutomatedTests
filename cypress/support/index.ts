ac8d72575aa8810e32ba3598c62bbd668dd24675
Aldrin Batac <aldrin.batac@leafnode.io>
2021-11-11 14:39:01 +0800

    Add cypress-real-events plugin. Cypress default events are simulated. That means that all events like cy.click or cy.type are fired from javascript. This will enable Cypress to fire real events (click, mouse, hover, etc).
    IBP-5195

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -14,7 +14,10 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
+
+// To support firing of native system events from Cypress
+import "cypress-real-events/support";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
