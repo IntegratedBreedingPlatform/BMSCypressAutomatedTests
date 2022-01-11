d35c7c1cb82c54f8db403dbbbd20e07201eab009
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2022-01-11 09:50:47 +0800

    Increased timeout
    IBP-5039

--- cypress/integration/pageobjects/release-notes-page.ts
+++ cypress/integration/pageobjects/release-notes-page.ts
@@ -20,7 +20,7 @@ export default class ReleaseNotesPage {
 
     verifyReleaseNotesHeaderDisplayed() {
         this.getReleaseNotesBody()
-            .find('jhi-main > div > section > jhi-release-notes-dialog > div.modal-header > h4 > span > span')
+            .find('jhi-main > div > section > jhi-release-notes-dialog > div.modal-header > h4 > span > span', {timeout:50000})
             .should('exist');
     }
     getReleaseNotesBody() {
