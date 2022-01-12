a24ec0ef93e126acc2fed0d61e0274e67eb636e6
Shiela Marie C. Barbacena <shiela.barbacena@leafnode.io>
2022-01-12 09:52:42 +0800

    Create import-filenames.ts

---
+++ cypress/integration/constants/import-filenames.ts
@@ -0,0 +1,6 @@
+export default class ImportFileNames{
+
+    public static readonly GERMPLASM_IMPORT_VALID = "GermplasmImport.xls"
+    public static readonly LIST_IMPORT_VALID = "GermplasmListImport.xls";
+    public static readonly LIST_IMPORT_NO_REQUIRED_COLS = "GermplasmListImportNoRequiredCol.xls"
+}
