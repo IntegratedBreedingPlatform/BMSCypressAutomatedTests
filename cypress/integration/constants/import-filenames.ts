ba64b9eccb4717b71201d7b0622c6ae565e6ddd1
Shiela Marie C. Barbacena <shiela.barbacena@leafnode.io>
2022-01-13 15:33:01 +0800

    Update import-filenames.ts
    add constant filename for import list with no values

--- cypress/integration/constants/import-filenames.ts
+++ cypress/integration/constants/import-filenames.ts
@@ -3,4 +3,5 @@ export default class ImportFileNames{
     public static readonly GERMPLASM_IMPORT_VALID = "GermplasmImport.xls"
     public static readonly LIST_IMPORT_VALID = "GermplasmListImport.xls";
     public static readonly LIST_IMPORT_NO_REQUIRED_COLS = "GermplasmListImportNoRequiredCol.xls"
+    public static readonly LIST_IMPORT_NO_VALUES = "GermplasmListImportNoValues.xls"
 }
