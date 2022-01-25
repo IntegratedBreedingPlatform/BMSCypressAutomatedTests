905ecac79bf0224f50a0253c5cfef4c7d11aaa27
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2022-01-25 14:39:54 +0800

    no message

--- cypress/integration/tests/manage-lists/clone-list/clone-list.defs.ts
+++ cypress/integration/tests/manage-lists/clone-list/clone-list.defs.ts
@@ -35,6 +35,7 @@ And('I add entry details variable to the list', () => {
     germplasmListPage.openAddEntryDetailsModal();
     germplasmListAddEntryDetailsPage.selectVariable();
     germplasmListPage.checkEntryDetailWasAdded();
+    germplasmListPage.checkGermplasmListTableHasColumn();
 });
 
 When('I clone the list', () => {
