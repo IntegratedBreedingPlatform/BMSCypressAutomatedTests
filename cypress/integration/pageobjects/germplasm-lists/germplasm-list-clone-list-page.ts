0704804c02ad244dec33c21aca0f52a49c43a843
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2022-01-25 17:51:13 +0800

    no message

--- cypress/integration/pageobjects/germplasm-lists/germplasm-list-clone-list-page.ts
+++ cypress/integration/pageobjects/germplasm-lists/germplasm-list-clone-list-page.ts
@@ -14,8 +14,8 @@ export default class GermplasmListCloneListPage {
     }
 
     verifyDuplicateNameErrorMessage() {
-        cy.intercept('POST', `**/germplasm-lists/search?*`).as('loadLists');
-        cy.wait('@loadLists',{ timeout: 60000}).then((interception) => {
+        cy.intercept('POST', `**/clone?*`).as('cloneList');
+        cy.wait('@cloneList',{ timeout: 60000}).then((interception) => {
             expect(interception.response.statusCode).to.be.equal(200);
             getIframeBody().find('ngb-alert > span',{ timeout: 120000})
                 .contains('There is an existing item with the same name you have entered. Please enter a different name.',{ timeout: 120000});
