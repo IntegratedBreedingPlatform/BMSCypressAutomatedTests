fb4d1123245151e6e0fc434780fcd13d6e8fa4f4
Darla Grace Hermo <darla@leafnode.io>
2022-02-21 13:43:16 +0800

    Streamline code for plot visualization to reuse existing/common steps
    IBP-5500

--- cypress/integration/pageobjects/studies/create-study-page.ts
+++ cypress/integration/pageobjects/studies/create-study-page.ts
@@ -159,36 +159,29 @@ export default class CreateStudyPage {
         this.manageSettingsModal('Add Traits', variable);
     }
 
-    startNewStudyWithObservations (studyName: string, studyDesc: string, studyType: string, objective: string, observationName: string) {
+    addTraitWithObservations(variable: string) {
+        this.addTrait(variable)
+        this.addTraitObservations(variable);
+    }
+
+    saveNewStudyWithRCBDDesign(studyName: string, studyDesc: string, studyType: string, objective: string) {
+        // Step 1 - create new study with basic details
         this.saveStudyWithBasicDetails(studyName, studyDesc, studyType, objective);
-        this.addStudySettings();
+        // Step 2 - select germplasm list
         this.addGermplasms();
+        // Step 3 - generate RCBD design
         this.goToExperimentalDesign(false);
         this.generateRCBDesign();
         this.confirmGenerateModal();
         this.checkGenerateDesignSuccess();
-        this.goToObservations();
-        this.addObservations(observationName);
+        // Step 4 - wait for study to reload
+        this.waitForStudyToLoad();
     }
 
-    goToObservations() {
-        getMainIframeDocumentWaitLoad();
 
-        cy.intercept('POST', `**/observationUnits/table?*`).as('loadObservations');
-        this.clickTab('Observations');
-    }
-
-    addObservations(observationName: string) {
-        cy.wait('@loadObservations').then((interception) => {
-            getIframeBody().xpath(`//div[@id='manage-study-tabs']//section-container[@heading='TRAITS']//span[text()='Add']`).should('be.visible').click();
-        }
-
-        cy.intercept('POST', `**/observationUnits/table?*`).as('addTraits');
-        this.manageSettingsModal('Add Traits', observationName);
-
+    addTraitObservations(observationName: string) {
         cy.wait('@addTraits').then((interception) => {
             expect(interception.response.statusCode).to.equal(200);
-            getIframeBody().xpath(`//th[text()='${observationName}']`).should('be.visible');
             this.setVariableValues(observationName);
         });
     }
--- cypress/integration/tests/manage-studies/generate-plot-visualization.feature
+++ cypress/integration/tests/manage-studies/generate-plot-visualization.feature
@@ -7,7 +7,10 @@ Feature: Generate Plot Visualization
 
 Background: 
     Given I am on the Manage Studies page of specified program
-    And I create a trial using randomized complete block design with treatment factors
+    And I opened a study with RCBD design
+    And I add GCol_E_1to9 trait to the study with observations
+    And I add ASI_Cmp_day trait to the study with observations
+
 
 @TestCaseKey=IBP-T621
 @smoke-test
@@ -15,7 +18,7 @@ Scenario: Check if user can generate plot visualization - auto
     When I navigate to plot visualization screen
     And I select Scatterplot as plot type
     And I select Auto as regression method
-    And I select variable on X and Y
+    And I select variable GCol_E_1to9 on X and ASI_Cmp_day on Y
     And I generate plot visualization
     Then a plot visualization graph should display
     
