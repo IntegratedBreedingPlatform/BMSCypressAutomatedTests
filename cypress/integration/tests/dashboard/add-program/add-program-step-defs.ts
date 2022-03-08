d2dd272eaff1991fb05e164872dc6c07efe1de74
cheryleigh <chery@leafnode.io>
2022-03-08 15:17:34 +0800

    change start date input to current date
    IBP-5498

--- cypress/integration/tests/dashboard/add-program/add-program-step-defs.ts
+++ cypress/integration/tests/dashboard/add-program/add-program-step-defs.ts
@@ -2,6 +2,7 @@ import { And, Then, When } from 'cypress-cucumber-preprocessor/steps';
 import AddProgramPage from '../../../pageobjects/add-program-page';
 import { randomString } from '../../../../support/commands';
 import NavbarSection from '../../../pageobjects/navbar-section';
+import { DatePipe } from '@angular/common';
 
 const navBar = new NavbarSection();
 const addProgramPage = new AddProgramPage();
@@ -25,7 +26,8 @@ And('I enter a program name',()=>{
 })
 
 And('I enter a program start date',()=>{
-    addProgramPage.enterProgramStartDate("2022-03-07");
+    const datePipe = new DatePipe('en-US');
+    addProgramPage.enterProgramStartDate(datePipe.transform(Date.now(), 'YYYY-MM-dd'));
 })
 
 And('I save program details',()=>{
