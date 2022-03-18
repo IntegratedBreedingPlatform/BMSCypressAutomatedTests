b626cd884745015f5dad907df6b4ab355ed8a6bb
Shiela Marie C. Barbacena <shiela.barbacena@leafnode.io>
2022-03-18 16:55:45 +0800

    Update dashboard-page.ts

--- cypress/integration/pageobjects/dashboard-page.ts
+++ cypress/integration/pageobjects/dashboard-page.ts
@@ -38,7 +38,7 @@ export default class DashboardPage{
     }
     selectProgram(){
         getIframeBody().then(($iframe) => {
-            cy.wrap($iframe).find('#programDropdown .select2-selection__rendered').should('be.visible').invoke('text').then((text) => {
+            cy.wrap($iframe).find('#programDropdown .select2-selection__rendered').should('exist').invoke('text').then((text) => {
                 // select2 doesn't trigger change when programName is already selected so it always selects the first option for some reason
                 // workaround: only perform selection when selected text is not yet the specified programName
                 if (text != this.programName) {
