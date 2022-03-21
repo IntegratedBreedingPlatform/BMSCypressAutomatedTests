e98a495b8bfd7d41009a5a1e1821cc4ee744055a
Shiela Marie C. Barbacena <shiela.barbacena@leafnode.io>
2022-03-21 15:11:02 +0800

    Update dashboard-page.ts

--- cypress/integration/pageobjects/dashboard-page.ts
+++ cypress/integration/pageobjects/dashboard-page.ts
@@ -46,7 +46,7 @@ export default class DashboardPage{
                    // this.getProgramsIframeBody().find('span.select2-results').contains('ul', this.programName).should('be.visible').click();
 
                     cy.wrap($iframe).find('input[role="searchbox"]').should('be.visible').type(this.programName);
-                    cy.wrap($iframe).find('span.select2-results').contains('li', this.programName).should('be.visible').click();
+                    cy.wrap($iframe).xpath('//body/span/span/span[2]/ul').contains('li', this.programName).should('be.visible').click();
                     //cy.wrap($iframe).find('#programDropdown .select2-selection__rendered').should('have.text',this.programName);
                 }
             });
