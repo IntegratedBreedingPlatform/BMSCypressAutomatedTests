bcb8c46c5a7e90e5ca1e3f0ed0eddedf76be4fa8
Shiela Marie C. Barbacena <shiela.barbacena@leafnode.io>
2022-05-17 15:31:36 +0800

    Update excel-data-generator.ts

--- cypress/plugins/tasks/excel-data-generator.ts
+++ cypress/plugins/tasks/excel-data-generator.ts
@@ -32,6 +32,23 @@ export function generateImportCrossesTestData (fileName: string) {
     let sheetName = workbook.SheetNames[0]; //get 'Lots' sheet
     let workSheet = workbook.Sheets[sheetName];
     XLSX.utils.sheet_add_aoa(workSheet, [[gid]], {origin: 'A2'});
+    //update method
+    XLSX.utils.sheet_add_aoa(workSheet, [['UBM']], {origin: 'C2'});  
+    //update progenitor
+    XLSX.utils.sheet_add_aoa(workSheet, [['1']], {origin: 'D2'});
+    XLSX.utils.sheet_add_aoa(workSheet, [['1']], {origin: 'E2'});
+    //update preferred name
+    XLSX.utils.sheet_add_aoa(workSheet, [['PUI']], {origin: 'F2'});    
+    //update location abbr
+    XLSX.utils.sheet_add_aoa(workSheet, [['AFG']], {origin: 'G2'});    
+    //update creation date
+    XLSX.utils.sheet_add_aoa(workSheet, [['20220101']], {origin: 'H2'});    
+    //update reference
+    XLSX.utils.sheet_add_aoa(workSheet, [['Reference1']], {origin: 'I2'});   
+    //update DRVNM
+    XLSX.utils.sheet_add_aoa(workSheet, [['Drvnm1']], {origin: 'J2'}); 
+    //update NOTE_AA_text
+    XLSX.utils.sheet_add_aoa(workSheet, [['Note1']], {origin: 'K2'});   
     //add PUI column and value
     XLSX.utils.sheet_add_aoa(workSheet, [['PUI']], {origin: 'L1'});
     XLSX.utils.sheet_add_aoa(workSheet, [['PUINewGermplasm1']], {origin: 'L2'});
@@ -44,24 +61,8 @@ export function generateImportCrossesTestData (fileName: string) {
     //update existing name
     XLSX.utils.sheet_add_aoa(workSheet, [['LNAME']], {origin: 'O1'});
     XLSX.utils.sheet_add_aoa(workSheet, [['NewGermplasm1Edited']], {origin: 'O2'});
+
     XLSX.writeFile(workbook, fileName); // write the same file with new values
-    //update progenitor
-    XLSX.utils.sheet_add_aoa(workSheet, [['1']], {origin: 'D2'});
-    XLSX.utils.sheet_add_aoa(workSheet, [['1']], {origin: 'E2'});
-    //update method
-    XLSX.utils.sheet_add_aoa(workSheet, [['UBM']], {origin: 'C2'});    
-    //update preferred name
-    XLSX.utils.sheet_add_aoa(workSheet, [['PUI']], {origin: 'F2'});    
-    //update location abbr
-    XLSX.utils.sheet_add_aoa(workSheet, [['AFG']], {origin: 'G2'});    
-    //update creation date
-    XLSX.utils.sheet_add_aoa(workSheet, [['20220101']], {origin: 'H2'});    
-    //update reference
-    XLSX.utils.sheet_add_aoa(workSheet, [['Reference1']], {origin: 'I2'});   
-    //update DRVNM
-    XLSX.utils.sheet_add_aoa(workSheet, [['Drvnm1']], {origin: 'J2'});   
-    //update NOTE_AA_text
-    XLSX.utils.sheet_add_aoa(workSheet, [['Note1']], {origin: 'K2'});  
     // explicitly return null to signal that the given event has been handled.
     return null;
   }
