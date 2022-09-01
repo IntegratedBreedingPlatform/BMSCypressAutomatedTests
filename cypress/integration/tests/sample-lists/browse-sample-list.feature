1aa0d04c0f3c78d55136588d6a7ca8c2a81120fb
cheryleigh <chery@leafnode.io>
2022-09-01 18:35:08 +0800

    IBP-5567: implement browse and select sample list automation

--- cypress/integration/tests/sample-lists/browse-sample-list.feature
+++ cypress/integration/tests/sample-lists/browse-sample-list.feature
@@ -9,8 +9,9 @@ Background:
   Given I am on the Manage Studies page of specified program
   And I opened a study with RCBD design
   And I add nEarsSel selection to the study with observations
+  And I click Create genotyping samples action
   And I create a genotyping sample list for the study
-  And I am on the Sample Lists page of specified program
+  And I am on the Samples Lists page of specified program
 
 @TestCaseKey=IBP-T109
 @smoke-test
--- cypress/integration/tests/sample-lists/browse-sample-list/browse-sample-list-step-defs.ts
+++ cypress/integration/tests/sample-lists/browse-sample-list/browse-sample-list-step-defs.ts
@@ -2,16 +2,17 @@ import { When, And, Then } from 'cypress-cucumber-preprocessor/steps';
 import { randomString } from '../../../../support/commands';
 import SampleListPage from '../../../pageobjects/studies/sample-list-page';
 
-const sampleListPage = new SampleListPage()
+const sampleListPage = new SampleListPage();
+const listName = 'SampleList' + randomString();
 
 And('I create a genotyping sample list for the study',  () => {
-
+    sampleListPage.createGenotypeSamples('nEarsSel', listName);
 });
 
 When('I browse and select a sample list',  () => {
-
+    sampleListPage.browseAndSelectList(listName);
 });
 
 Then('I should be able to see the details of the sample list',  () => {
-
+    sampleListPage.verifyListDetails(listName);
 });
