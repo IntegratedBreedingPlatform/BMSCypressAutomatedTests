fef892c3b3686c5e12b13dcdaad7a8ef994e3119
cheryleigh <chery@leafnode.io>
2022-12-15 21:50:40 +0800

    IBP-6099: add checking for retrieved data

--- cypress/integration/pageobjects/inventory/view-lots-page.ts
+++ cypress/integration/pageobjects/inventory/view-lots-page.ts
@@ -1,8 +1,16 @@
 import { getIframeBody } from '../../../support/commands';
 
 export default class ViewLotsPage{
+
     verifyLotDetailsDisplayed() {
+        cy.intercept('GET', `bmsapi/crops/${Cypress.env('cropName')}/lots/search?programUUID=*`).as('lotsSearch');
         getIframeBody().find('[data-test="lotDetailDialog"]').should('exist');
+
+        cy.wait('@lotsSearch').then((interception) => {
+            expect(interception.response?.statusCode).to.be.equal(200);
+            const respBody = interception.response?.body[0];
+            expect(respBody).property('designation').to.be.oneOf(["TestGP1", "TestGP2", "TestGP3"])
+        });
     }
 
     clickLotStockId() {
