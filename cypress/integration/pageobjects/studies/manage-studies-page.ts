35ec6dc28bbccc843c15d406d774dd7d452c3ec4
Shiela Marie C. Barbacena <barbacenasmc@gmail.com>
2023-03-06 16:22:40 +0800

    Update manage-studies-page.ts

--- cypress/integration/pageobjects/studies/manage-studies-page.ts
+++ cypress/integration/pageobjects/studies/manage-studies-page.ts
@@ -13,7 +13,7 @@ export default class ManageStudiesPage {
     }
 
     startNewStudy() {
-       getMainIframeDocumentWaitToLoad().find('[jhitranslate="study.manager.actions.new-study"]').should('exist').should('be.enabled').click({force:true});
+       getMainIframeDocumentWaitToLoad().find('[data-test="startNewStudyButton"]').should('exist').should('be.enabled').click({force:true});
     }
 }
 
